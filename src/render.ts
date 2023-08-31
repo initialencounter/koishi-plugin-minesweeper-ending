52918c8365c2c45ef82cf8d191206e269b77fac0
initialencounter <2911583893@qq.com>
2023-09-01 02:21:58 +0800

    change: render

--- src/render.ts
+++ src/render.ts
@@ -1,46 +1,79 @@
 import { resolve } from "path";
-const fs = require('fs');
-
+import fs from 'fs';
+import { PNG } from 'pngjs';
 
 const theme = "wom"
-const themePath = resolve(__dirname,"../../../data/minesweeper/theme/",theme)
-// const closedPng = fs.readFileSync(resolve(themePath,"closed.png"))
-const flagPng = fs.readFileSync(resolve(themePath,"flag.png"))
-// const type0Png = fs.readFileSync(resolve(themePath,"type0.png"))
-const type1Png = fs.readFileSync(resolve(themePath,"type1.png"))
-// const type2Png = fs.readFileSync(resolve(themePath,"type2.png"))
-// const type3Png = fs.readFileSync(resolve(themePath,"type3.png"))
-// const type4Png = fs.readFileSync(resolve(themePath,"type4.png"))
-// const type5Png = fs.readFileSync(resolve(themePath,"type5.png"))
-// const type6Png = fs.readFileSync(resolve(themePath,"type6.png"))
-// const type7Png = fs.readFileSync(resolve(themePath,"type7.png"))
-// const type8Png = fs.readFileSync(resolve(themePath,"type8.png"))
+const themePath = resolve(__dirname, "../../../data/minesweeper/theme/", theme)
 
+async function readImageAsArray(imagePath:string) {
+    const imageStream = fs.createReadStream(imagePath);
+    const png = imageStream.pipe(new PNG());
+    
+    await new Promise((resolve, reject) => {
+        png.on('parsed', resolve).on('error', reject);
+    });
 
-// // 读取图片文件的二进制数据
-// const image1Data = fs.readFileSync('path/to/image1.png');
-// const image2Data = fs.readFileSync('path/to/image2.png');
+    const width = png.width;
+    const height = png.height;
+    const pixels = new Array(height);
+    
+    for (let y = 0; y < height; y++) {
+        pixels[y] = new Array(width);
+        for (let x = 0; x < width; x++) {
+            const idx = (width * y + x) << 2;
+            const r = png.data[idx];
+            const g = png.data[idx + 1];
+            const b = png.data[idx + 2];
+            const a = png.data[idx + 3];
+            pixels[y][x] = [r, g, b, a];
+        }
+    }
+    return pixels;
+}
+const imageTypes = ['closed', 'flag', 'type0', 'type1', 'type2', 'type3', 'type4', 'type5', 'type6', 'type7', 'type8'];
 
-// // 将二进制数据转换为Uint8Array
-const image1Array = new Uint8Array(flagPng);
-const image2Array = new Uint8Array(type1Png);
+async function loadImages(themePath) {
+    const imagePromises = imageTypes.map(type => readImageAsArray(resolve(themePath, `${type}.png`)));
+    return await Promise.all(imagePromises);
+}
 
-// 创建一个新的Uint8Array来存储拼接后的图像数据
-const combinedImageArray = new Uint8Array(image1Array.length + image2Array.length);
-combinedImageArray.set(image1Array,0);
-combinedImageArray.set(image2Array, image1Array.length);
+async function writeArrayToImage(pixels, width, height, imagePath) {
+    // 创建一个新的PNG对象
+    const png = new PNG({ width, height });
+    // 将像素数据写入PNG对象
+    for (let y = 0; y < height; y++) {
+        for (let x = 0; x < width; x++) {
+            // 获取像素索引
+            const idx = (width * y + x) << 2;
+            // 获取像素的RGBA值
+            const [r, g, b, a] = pixels[y][x];
+            // 将像素数据写入PNG对象
+            png.data[idx] = r;
+            png.data[idx + 1] = g;
+            png.data[idx + 2] = b;
+            png.data[idx + 3] = a;
+        }
+    }
+    // 将PNG对象写入文件
+    png.pack().pipe(fs.createWriteStream(imagePath));
+}
+function concatX(...arrayx: number[][][][]) {
+    const temp: number[][][] = []
+    for (let i = 0; i < 94; i++) {
+        const temp2: number[][] = []
+        const subArrays = arrayx.map(subArray => subArray[i]);
+        for (const subArray of subArrays) {
+            for (const item of subArray) {
+                temp2.push(item);
+            }
+        }
+        temp.push(temp2)
+    }
+    return temp
+}
 
-// 将拼接后的图像数据写入新的图片文件
-fs.writeFileSync('combined.png', Buffer.from(combinedImageArray));
-
-console.log('拼接完成！');
-async function mergeImages(image1Path, image2Path, x, y) {
-    // 读取第一张图片
-    const image1 = await Jimp.read(image1Path);
-    // 读取第二张图片
-    const image2 = await Jimp.read(image2Path);
-    // 将第二张图片合并到第一张图片上
-    image1.composite(image2, x, y);
-    // 返回合并后的图片
-    return image1;
-  }
+async function main() {
+    const [closedPng, flagPng, type0Png, type1Png, type2Png, type3Png, type4Png, type5Png, type6Png, type7Png, type8Png] = await loadImages(themePath);
+    const img = concatX(closedPng, flagPng, type0Png, type1Png, type2Png, type3Png, type4Png, type5Png, type6Png, type7Png, type8Png)
+    await writeArrayToImage(img, img[0].length, img.length, "test.png")
+}
